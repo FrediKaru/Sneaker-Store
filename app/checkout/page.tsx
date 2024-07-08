d01d6a0757f79da84a0ebb75a8a110e5f18598e3
Fredi Karu <fredikaru@gmail.com>
2024-07-08 20:33:21 +0300

    Fix type error

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -57,7 +57,7 @@ export default function Checkout() {
                         Select Size
                       </label>
                       <select className="border p-2">
-                        <option defaultValue>Choose size</option>
+                        <option value="">Choose size</option>
                         <option value="EU40">EU 40</option>
                         <option value="EU42">EU 42</option>
                         <option value="EU46">EU 46</option>
